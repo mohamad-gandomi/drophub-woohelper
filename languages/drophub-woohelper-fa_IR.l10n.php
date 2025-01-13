d66d8c4cfdb69d3183c0dc340b3c273abaf29053
gandomi <m.gandomi1995@gmail.com>
2025-01-13 12:28:24 +0330

    add empty js file for error, minor css changes, update fa translate files

--- languages/drophub-woohelper-fa_IR.l10n.php
+++ languages/drophub-woohelper-fa_IR.l10n.php
@@ -1,2 +1,9 @@
 <?php
-return ['project-id-version'=>'DropHub WooHelper','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-12-29 12:52+0000','po-revision-date'=>'2024-12-29 13:53+0000','last-translator'=>'mg-shop','language-team'=>'Persian','language'=>'fa_IR','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.14; wp-6.7.1','x-domain'=>'drophub-woohelper','messages'=>['Estimated shipping: %1$d-%2$d days'=>'زمان تقریبی ارسال: %1$d-%2$d روز','Estimated shipping: %d days'=>'زمان تقریبی ارسال: %d روز','Return Policy'=>'قوانین بازگشت','Sorry, %s cannot be shipped to %s'=>'متأسفیم، ارسال %s به %s امکان‌پذیر نیست.']];
+return ['project-id-version'=>'DropHub WooHelper','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-12-29 12:52+0000','po-revision-date'=>'2025-01-13 08:47+0000','last-translator'=>'Arch The Body','language-team'=>'Persian','language'=>'fa_IR','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.14; wp-6.7.1','x-domain'=>'drophub-woohelper','messages'=>['Cash on Delivery'=>'پرداخت در زمان تحویل','Delivery Time: %d-%d days'=>'زمان تحویل: %d تا %d روز','Delivery: %d-%d days'=>'تحویل: %d تا %d روز','Fallback Shipping'=>'ارسال جایگزین','Pay Upon Delivery'=>'پرداخت در زمان تحویل
+
+
+
+
+
+
+','Return Policy'=>'قوانین بازگشت','Shipping Cost'=>'هزینه ارسال','Shipping Cost: %s'=>'هزینه ارسال: %s','Sorry, %s cannot be shipped to %s'=>'متأسفیم، ارسال %s به %s امکان‌پذیر نیست.','Total Shipping: %s'=>'هزینه حمل و نقل: %s']];
